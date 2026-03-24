0ed77a2c0581874d6472fb7191b35b94019b27e8
tony <unotony333@yahoo.com.tw>
2026-03-24 13:54:19 +0800

    Initial Commit

---
+++ AI GM/AI_GMApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_GMApp.swift
+//  AI GM
+//
+//  Created by tony on 2026/3/24.
+//
+
+import SwiftUI
+
+@main
+struct AI_GMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
